4f1d5cc55eb3c3be91428354d1fdffdaaa9ea6c9
Sandner <wsandner@1usmwsandner.local>
2016-01-03 19:38:17 +0100

    unhide dotfiles in this directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,9 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files=".bash_aliases .bash_profile .bashrc .dir_colors .vimrc .vim \
-       .git_svn_bash_prompt .gitconfig .gitignore_global \
-       .mongorc.js .vim .vimrc"        
+files="bash_aliases bash_profile bashrc dir_colors vimrc vim \
+       git_svn_bash_prompt gitconfig gitignore_global \
+       mongorc.js vim vimrc"        
 
 ##########
 
@@ -16,16 +16,12 @@ mkdir -p $olddir
 echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/$file $olddir
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
+    ln -s $dir/$file ~/.$file
 done
 
 source ~/.bash_profile
